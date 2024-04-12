233652ac8bc8217b4917a455d648f19091af4496
Yeikol Lorenzo <yeigamer1@gmail.com>
2024-04-11 20:21:17 -0400

    CORE: FIX ConsultaDto

--- caresoft_core/caresoft_core/Dto/ConsultaDto.cs
+++ caresoft_core/caresoft_core/Dto/ConsultaDto.cs
@@ -4,21 +4,21 @@ public class ConsultaDto
 {
     public string ConsultaCodigo { get; set; } = null!;
 
-    public string? DocumentoPaciente { get; set; } = null!;
+    public string DocumentoPaciente { get; set; } = null!;
 
-    public string? DocumentoMedico { get; set; } = null!;
+    public string DocumentoMedico { get; set; } = null!;
 
-    public uint? IdConsultorio { get; set; }
+    public uint IdConsultorio { get; set; }
 
-    public uint? IdAutorizacion { get; set; }
+    public uint IdAutorizacion { get; set; }
 
-    public DateTime? Fecha { get; set; }
+    public DateTime Fecha { get; set; }
 
-    public string? Motivo { get; set; } = null!;
+    public string Motivo { get; set; } = null!;
 
     public string? Comentarios { get; set; }
 
-    public string? Estado { get; set; } = null!;
+    public string Estado { get; set; } = null!;
 
-    public decimal? Costo { get; set; }
+    public decimal Costo { get; set; }
 }
