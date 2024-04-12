84057298da15fd479a1078a2b2acbcea5d421f53
David Mendoza <davidalejandromenver@gmail.com>
2024-04-11 21:33:26 -0400

    IngresoDto creacion

---
+++ caresoft_core/caresoft_core/Dto/IngresoDto.cs
@@ -0,0 +1,16 @@
+﻿namespace caresoft_core.Dto
+{
+    public class IngresoDto
+    {
+        public uint? IdIngreso { get; set; }
+        public string DocumentoPaciente { get; set; }
+        public string DocumentoEnfermero { get; set; }
+        public string DocumentoMedico { get; set; }
+        public string? ConsultaCodigo { get; set; }
+        public uint? IdAutorizacion { get; set; }
+        public uint NumSala { get; set; }
+        public decimal CostoEstancia { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public DateTime? FechaAlta { get; set; }
+    }
+}
